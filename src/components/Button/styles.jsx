2df59d0d65db7a0a281cd8ec1605e9210ee3e06e
Luiz Falcao <luizotavio-falcao@hotmail.com>
2021-10-31 16:53:21 -0300

    Fix button aspect ratio

--- src/components/Button/styles.jsx
+++ src/components/Button/styles.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Button = styled.button`
   color: #fff;
   padding: 0.5rem;
+  min-height: 3rem;
   cursor: pointer;
   border: 1px dashed transparent;
   transition: 0.15s ease-in-out;
@@ -23,7 +24,6 @@ export const Button = styled.button`
 export const TextBtn = styled(Button)`
   color: #fff;
   font-weight: 600;
-  min-height: 3rem;
   padding: 0 1.5rem;
   border-radius: 0.5rem;
 `;
